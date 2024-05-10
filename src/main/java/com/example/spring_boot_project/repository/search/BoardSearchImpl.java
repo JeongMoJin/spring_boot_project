528813bb02d411a3ab7ee6f6234daae65c606fee
LearnAndRecord <121412089+JeongMoJin@users.noreply.github.com>
2024-05-10 13:00:27 +0900

    Update BoardSearchImpl.java
    2024-05-10

--- src/main/java/com/example/spring_boot_project/repository/search/BoardSearchImpl.java
+++ src/main/java/com/example/spring_boot_project/repository/search/BoardSearchImpl.java
@@ -1,6 +1,7 @@
 package com.example.spring_boot_project.repository.search;
 
 import com.example.spring_boot_project.domain.Board;
+import com.example.spring_boot_project.domain.QBoard;
 import com.querydsl.jpa.JPQLQuery;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
